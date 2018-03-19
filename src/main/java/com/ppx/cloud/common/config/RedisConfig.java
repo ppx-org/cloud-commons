aba3b78e6625c0ea3caa44f46a5914fcb7227735
dengxz <13602493280@qq.com>
2018-03-19 08:49:39 +0800

    t

--- src/main/java/com/ppx/cloud/common/config/RedisConfig.java
+++ src/main/java/com/ppx/cloud/common/config/RedisConfig.java
@@ -7,7 +7,6 @@ import javax.annotation.Resource;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
